0bc8cb9b5e3e9fc46be7eea41cf9fb88f2eb7e2a
Steven Copeland <steven@thomasleecopeland.com>
2017-08-09 12:15:11 -0400

    adding defualt version

---
+++ spec/sourcefetch-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SourcefetchPackageView from '../lib/sourcefetch-package-view';
+
+describe('SourcefetchPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
